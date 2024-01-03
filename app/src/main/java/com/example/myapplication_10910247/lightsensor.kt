1b8a5beef150192d71cc217f76b8b6cb08e01ea0
10810208 <10810208@gm.chihlee.edu.tw>
2024-01-03 18:32:29 +0800

    返回主畫面可使用

--- app/src/main/java/com/example/myapplication_10910247/lightsensor.kt
+++ app/src/main/java/com/example/myapplication_10910247/lightsensor.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.myapplication_10910247.databinding.ActivityMainBinding
 
 class lightsensor : AppCompatActivity() {
 
@@ -11,6 +12,7 @@ class lightsensor : AppCompatActivity() {
     private lateinit var back123: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_lightsensor)
 
         val startServiceButton: Button = findViewById(R.id.light)
@@ -18,7 +20,19 @@ class lightsensor : AppCompatActivity() {
             val serviceIntent= Intent (this,myservice::class.java);
             startService(serviceIntent)
 
+        val startActivityButton: Button=findViewById(R.id.back123)
+            startActivityButton.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
 
         }
+
+    }
+
+    /*override fun onBackPressed() {
+        super.onBackPressed()*/
+
+
     }
 }
