e1b91f435f6bae9dc881ff07dc9d591f6b3d4348
10810208 <10810208@gm.chihlee.edu.tw>
2024-01-03 16:49:19 +0800

    建立myservice

---
+++ app/src/main/java/com/example/myapplication_10910247/myservice.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication_10910247
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class myservice : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+
+    }
+}
